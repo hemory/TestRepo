c7dfb238ad361ad31c07f05cb74c89575e164ac6
hemory <hphife@yahoo.com>
2020-06-21 09:20:29 -0400

    First Commit

---
+++ ForLoopPractice/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int userAge = Convert.ToInt16(Console.ReadLine());
+
+            bool isOfLegalAge = IsLegal(userAge);
+
+            Console.WriteLine(isOfLegalAge);
+            
+            Console.ReadLine();
+        }
+
+
+
+        
+        public static void CountUp(int maxNumber)
+        {
+            for (int i = 0; i <= maxNumber; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static List<int> GenerateAListOfNumbers()
+        {
+            List<int> listOfNumbers = new List<int> {1,2,3,4,5,6,7,8,9};
+
+            return listOfNumbers;
+        }
+
+        public static bool IsLegal(int age)
+        {
+            if (age >= 18)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
